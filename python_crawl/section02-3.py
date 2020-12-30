f07eb4f014a868f705cfa6efe8c2ab0687d25e46
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-12-30 23:38:38 +0900

    lxml 사용 기초 스크래핑 A 3

--- python_crawl/section02-3.py
+++ python_crawl/section02-3.py
@@ -17,11 +17,11 @@ def main():
     # 신문사 링크 리스트 획득
     urls = scrape_news_list_page(response)
 
-    # # 결과 출력
-    # for url in urls:
-    #     print(url)
-    #     # 파일 쓰기
-    #     # 생략
+    # 결과 출력
+    for url in urls:
+        print(url)
+        # 파일 쓰기
+        # 생략
 
 
 def scrape_news_list_page(response):
@@ -31,9 +31,12 @@ def scrape_news_list_page(response):
     # 태그 정보 문자열 저장
     root = lxml.html.fromstring(response.content)
 
-    for a in root.cssselect(""):
+    for a in root.cssselect(".group_news .thumb_area div.popup_wrap a.btn_popup[href*=http]"):
         # 링크
-        pass
+        url = a.get("href")
+        urls.append(url)
+
+    return urls
 
 
 # 스크랩핑 시작
