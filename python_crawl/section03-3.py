2799f3cdd256cb8c410529a3fcff37c146aa0d49
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-20 23:28:41 +0900

    기본 스크랩핑 실습- 04. 다음사이트 주식 정보 가져오기 - 1

---
+++ python_crawl/section03-3.py
@@ -0,0 +1,15 @@
+# Section03-3
+# 기본 스크랩핑 실습
+# 다음 주식 정보 가져오기
+
+import json
+import urllib.request as req
+from fake_useragent import UserAgent
+
+print(1)
+ua = UserAgent()
+# print(ua.ie)
+# print(ua.msie)
+# print(ua.chrome)
+# print(ua.safari)
+print(ua.random)
