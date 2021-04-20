e7798a19499be17e917208d3ed93fcad837c3271
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-20 23:50:29 +0900

    기본 스크랩핑 실습- 05. 다음사이트 주식 정보 가져오기 - 2

--- python_crawl/section03-3.py
+++ python_crawl/section03-3.py
@@ -6,10 +6,22 @@ import json
 import urllib.request as req
 from fake_useragent import UserAgent
 
-print(1)
+# Fake Header 정보 (가상으로 User-agent 생성)
 ua = UserAgent()
 # print(ua.ie)
 # print(ua.msie)
 # print(ua.chrome)
 # print(ua.safari)
-print(ua.random)
+# print(ua.random)
+
+# 헤더 정보
+headers = {
+    "User-agent": ua.ie,
+    "referer": "https://finance.daum.net/",
+}
+
+# 다음 주식 요청 URL
+url = "https://finance.daum.net/api/search/ranks?limit=10"
+
+# 요청
+res = req.urlopen(req.Request(url, headers=headers))
