ea41ebac3b2a07820bd03fb2833357bed3b27d0d
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-21 13:59:15 +0900

    Request - 04. request 사용 스크랩핑 B - 2
    수신 데이터 JSON 출력

--- python_crawl/section04-2.py
+++ python_crawl/section04-2.py
@@ -12,7 +12,7 @@ s = requests.Session()
 r = s.get("https://httpbin.org/stream/100", stream=True)
 
 # 수신 확인
-print(r.text)
+# print(r.text)
 
 # Encoding 확인
 print("Before Encoding : {}".format(r.encoding))
@@ -21,3 +21,48 @@ if r.encoding is None:
     r.encoding = "UTF-8"
 
 print("After Encoding : {}".format(r.encoding))
+
+for line in r.iter_lines(decode_unicode=True):
+    # 라인 출력 후 타입 확인
+    # print(line)
+    # print(type(line))
+
+    # JSON(Dict) 변환 후 타입 확인
+    b = json.loads(line)  # str -> dict
+    # print(b)
+    # print(type(b))
+
+    # 정보 내용 출력
+    for k, v in b.items():
+        print(f"Key : {k}, value : {v}")
+
+    print("\n")
+
+s.close()
+
+# -----------------------------------------
+
+r = s.get("https://jsonplaceholder.typicode.com/todos/1")
+
+# Header 정보
+# print(r.headers)
+
+# 본문 정보
+print(r.text)
+
+# json 변환
+print(r.json())
+
+# key 반환
+print(r.json().keys())
+
+# value 반환
+print(r.json().values())
+
+# 인코딩 반환
+print(r.encoding)
+
+# 바이너리 정보
+print(r.content)
+
+s.close()
