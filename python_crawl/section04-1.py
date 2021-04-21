143d5c93419aa387d23d8730505e5b65f5ca7853
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-21 12:29:52 +0900

    Request - 02. request 사용 스크랩핑 A - 2
    httpbin.org 테스트,
    쿠키 정보 전송,
    세션 활성화 비활성화

--- python_crawl/section04-1.py
+++ python_crawl/section04-1.py
@@ -21,4 +21,30 @@ import requests
 # s.close()
 
 # -------------------------------------------------------------------
+s = requests.Session()
+
 # 쿠키 Return
+r1 = s.get("https://httpbin.org/cookies", cookies={"name": "kim1"})
+print(r1.text)
+
+# 쿠키 Set
+r2 = s.get("https://httpbin.org/cookies/set", cookies={"name": "kim2"})
+print(r2.text)
+
+# User-Agent
+url = "https://httpbin.org"
+headers = {"user-agent": "nice-man_1.0.0_win10_ram16_home_chrome"}
+
+# Header 정보 전송
+r3 = s.get(url, headers=headers)
+print(r3.text)
+
+# 세션 비활성화
+s.close()
+
+# with문 사용(권장) -> 파일, DB, HTTP (외부 리소스 사용시)
+# 가독성, 유지보수, 실수 방지 의 장점
+with requests.Session() as s:
+    r = s.get("https://daum.net")
+    print(r.text)
+    print(r.ok)
